f6985bc1cbd6267eb5948a3a661274925a0215df
Ashley Schultz <ashleyeschultz@gmail.com>
2018-04-17 18:46:23 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,8 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  array.index("am")
+  index1 = array.index[0]
+  index
 end
 
 def retrieve_last_element_from_array(array)
